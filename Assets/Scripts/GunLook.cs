b219c638033aba5aa9288715ef96632737ee3fbb
Tristan <trimoore5@gmail.com>
2020-11-12 20:33:03 +1100

    Beginning Gun Look Script

---
+++ Assets/Scripts/GunLook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLook : MonoBehaviour
+{
+    public Transform retical;
+
+    void Update()
+    {
+
+    }
+}
