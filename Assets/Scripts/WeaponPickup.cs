91f09fe02d18adc5d26bed18cb5e2882505a843e
Storm101 <trimoore5@gmail.com>
2020-11-16 20:35:20 +1100

    Added Weapons & Inventory System

---
+++ Assets/Scripts/WeaponPickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+    public GameObject weapon;
+
+    private WeaponUse weaponScript;
+
+    void Start()
+    {
+        weaponScript = GameObject.Find("Player").GetComponent<WeaponUse>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (weapon == GameObject.Find("GunPickup"))
+        {
+            weaponScript.hasGun = true;
+            weapon.SetActive(false);
+        }
+
+        if (weapon == GameObject.Find("DaggerPickup"))
+        {
+            weaponScript.hasDagger = true;
+            weapon.SetActive(false);
+        }
+    }
+}
