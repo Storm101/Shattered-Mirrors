b9c2f6c9cd22200bc729a095b9ff9e4a65c8447b
Storm101 <trimoore5@gmail.com>
2020-11-28 13:25:10 +1100

    Update InnerThoughtDialogue.cs

--- Assets/Player/Scripts/InnerThoughtDialogue.cs
+++ Assets/Player/Scripts/InnerThoughtDialogue.cs
@@ -4,4 +4,36 @@ using UnityEngine;
 
 public class InnerThoughtDialogue : MonoBehaviour
 {
+    public string text;
+    public GameObject triggerText;
+
+    public float time;
+    public float timeIWant;
+    public bool timerActive;
+
+
+    void Start()
+    {
+        triggerText = GameObject.Find(text);
+        timeIWant = 2;
+    }
+
+    void OnTriggerEnter(Collider innerThought)
+    {
+        triggerText.SetActive(true);
+
+        timerActive = true;
+        timer();
+    }
+
+    void timer()
+    {
+        if (timerActive)
+            time = Time.deltaTime;
+        if (time >= timeIWant)
+        {
+            timerActive = false;
+            time = 0;
+        }
+    }
 }
