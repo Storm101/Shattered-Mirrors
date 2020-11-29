b5b4cbb923f5c35ada7ac9264c65e0233a67e641
Storm101 <trimoore5@gmail.com>
2020-11-29 19:16:14 +1100

    Added Doors & Lighting

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+    bool timerStarted = false;
+
+    public Animator leftDoorAnim;
+    public Animator rightDoorAnim;
+
+    void Start()
+    {
+        rightDoorAnim.SetBool("RightDoorOpen", false);
+        leftDoorAnim.SetBool("LeftDoorOpen", false);
+    }
+
+    void Update()
+    {
+        if (timerStarted)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= TimeIWant)
+            {
+                _timer = 0f;
+                timerStarted = false;
+                rightDoorAnim.SetBool("RightDoorOpen", false);
+                leftDoorAnim.SetBool("LeftDoorOpen", false);
+            }
+        }
+    }
+
+    public float _timer = 0f;
+    public float TimeIWant = 2f;
+
+    void OnTriggerStay(Collider door)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            rightDoorAnim.SetBool("RightDoorOpen", true);
+            leftDoorAnim.SetBool("LeftDoorOpen", true);
+            if (!timerStarted)
+            {
+                timerStarted = true;
+            }
+        }
+    }
+}
